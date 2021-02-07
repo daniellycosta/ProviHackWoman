5223e25a406d2b7ede319b1f1446ddf122223e62
Alyne <42252701+alynensilva@users.noreply.github.com>
2021-02-06 23:13:08 -0300

    feat: cria página de cadastro

--- src/components/Cadastro/CadastroComponent.jsx
+++ src/components/Cadastro/CadastroComponent.jsx
@@ -7,40 +7,25 @@ export const CadastroComponent = (props) => {
   <Form.Group controlId="formBasicEmail">
     <Form.Label>Nome</Form.Label>
     <Form.Control type="name" placeholder="Nome*" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
   </Form.Group>
 
   <Form.Group controlId="formBasicEmail">
-    <Form.Label>Email </Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
+    <Form.Label>Sobrenome </Form.Label>
+    <Form.Control type="email" placeholder="Sobrenome" />
   </Form.Group>
-
 
   <Form.Group controlId="formBasicEmail">
-    <Form.Label>Email </Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
+    <Form.Label>E-mail </Form.Label>
+    <Form.Control type="email" placeholder="E-mail" />
   </Form.Group>
-
-
-
 
   <Form.Group controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
+    <Form.Label>Senha</Form.Label>
+    <Form.Control type="password" placeholder="Senha" />
   </Form.Group>
-  <Form.Group controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
+
   <Button variant="primary" type="submit">
-    Submit
+    Enviar
   </Button>
 </Form>
     
