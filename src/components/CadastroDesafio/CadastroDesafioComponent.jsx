1a5eeaee996b846e1e2bef8abeff65c948ba1893
luaanaapreiraa <luanapdsantos@gmail.com>
2021-02-07 16:08:23 -0300

    feat: tira o header

--- src/components/CadastroDesafio/CadastroDesafioComponent.jsx
+++ src/components/CadastroDesafio/CadastroDesafioComponent.jsx
@@ -3,47 +3,48 @@ import { Form, FormControl, Container, Jumbotron, Button } from "react-bootstrap
 
 export const CadastroDesafioComponent = (props) => {
   return (
+    <div>
 
-    <Container>
-      <h1>Cadastro de desafio</h1>
+      <Container>
+        <h1>Cadastro de desafio</h1>
 
-      <Jumbotron>
-        <br />
-        <h3>Preencha os dados abaixo</h3>
-        <Form>
-          <Form.Group controlId="nomeEmpresa">
-            <Form.Label>Nome da empresa*</Form.Label>
-            <Form.Control type="input" />
-          </Form.Group>
-
-          <Form.Group controlId="nomeDesafio">
-            <Form.Label>Título* </Form.Label>
-            <Form.Control type="input" />
-          </Form.Group>
-
-          <Form.Group controlId="textoDesafio">
-            <Form.Label>Descrição*</Form.Label>
-            <Form.Control as="textarea" rows={3} />
-          </Form.Group>
-
-          <Form.Control as="select">
-            <option>Escolha a área</option>
-            <option>Frontend</option>
-            <option>Backend</option>
-            <option>Ux/UI</option>
-          </Form.Control>
+        <Jumbotron>
           <br />
+          <h3>Preencha os dados abaixo</h3>
+          <Form>
+            <Form.Group controlId="nomeEmpresa">
+              <Form.Label>Nome da empresa*</Form.Label>
+              <Form.Control type="input" />
+            </Form.Group>
 
-          <Button variant="primary" type="submit">
-            Enviar
+            <Form.Group controlId="nomeDesafio">
+              <Form.Label>Título* </Form.Label>
+              <Form.Control type="input" />
+            </Form.Group>
+
+            <Form.Group controlId="textoDesafio">
+              <Form.Label>Descrição*</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+
+            <Form.Control as="select">
+              <option>Escolha a área</option>
+              <option>Frontend</option>
+              <option>Backend</option>
+              <option>Ux/UI</option>
+            </Form.Control>
+            <br />
+
+            <Button variant="primary" type="submit">
+              Enviar
         </Button>
 
-        </Form>
+          </Form>
 
-      </Jumbotron>
-    </Container>
+        </Jumbotron>
+      </Container>
 
-
+    </div>
 
 
 
